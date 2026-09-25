cd1fde8731e68d0ccb3ab248f0f74d6415e2bfbd
nixtratrade-commits <nixtratrade@gmail.com>
2026-09-26 00:10:08 +0330

    Fix payment receipt function

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ async def check_free_channel(update: Update, context: ContextTypes.DEFAULT_TYPE)
             "⚠️ امکان بررسی عضویت وجود نداشت. دوباره امتحان کن.",
             show_alert=True
         )
-    async def payment_receipt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def payment_receipt(update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.effective_user
 
         await update.message.reply_text(
