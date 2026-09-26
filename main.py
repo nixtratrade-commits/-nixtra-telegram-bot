30c04cd8f0c3e2a3c2b1639514d043b523e3de0c
nixtratrade-commits <nixtratrade@gmail.com>
2026-09-26 15:08:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await update.message.reply_text(
                     "❌ اشتراک شما به پایان رسیده است."
                 )
-        else:
+            else:
                 total_days = (now - start_date).days + 1
                 remaining_days = max(0, (end_date - now).days)
 
